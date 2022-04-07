74ff32ae6b6dfac7941109f86752e1d40a429ce6
Felix <felixm@usalinksystem.com>
2022-04-07 16:28:22 +0300

    add some additional inputs

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,36 @@ function App() {
     e.preventDefault();
     alert("Вы сабмитнули форму")
   }
+  const onClickDate = e => {
+    try {
+      e.target.showPicker();
+    } catch(e){}
+  }
   const changeErrorInputsState = () => setErrorState(!errorState);
   return (
     <div className="App">
-      <h1>Export portal inputs example</h1>
+      <h1>EP inputs example</h1>
       <button type="button" onClick={changeErrorInputsState}>Show error inputs state</button>
       <form action="/?default" method="get" onSubmit={onSubmit} className={errorState ? "errorState" : ""}>
-        <input placeholder={`Input default without type`}></input>
-        <input type="text" placeholder={`Input type="text"`}></input>
-        <input type="email" placeholder={`Input type="email"`}></input>
-        <input type="search" placeholder={`Input type="search"`}></input>
-        <input type="tel" placeholder={`Input type="tel"`}></input>
-        <input type="url" placeholder={`Input type="url"`}></input>
-        <input type="number" placeholder={`Input type="number"`}></input>
-        <input type="password" placeholder={`Input type="password"`}></input>
+        <input placeholder={`Input default without type`}/>
+        <input type="text" placeholder={`Input type="text"`}/>
+        <input type="email" placeholder={`Input type="email"`}/>
+        <input type="search" placeholder={`Input type="search"`}/>
+        <input type="tel" placeholder={`Input type="tel"`}/>
+        <input type="url" placeholder={`Input type="url"`}/>
+        <input type="number" placeholder={`Input type="number"`}/>
+        <input type="password" placeholder={`Input type="password"`}/>
+        <input type="color"/>
+        <input type="range"/>
+        <input onClick={onClickDate} type="date"/>
+        <input onClick={onClickDate} type="datetime-local"/>
+        <input onClick={onClickDate} type="month"/>
+        <input onClick={onClickDate} type="week"/>
+        <input onClick={onClickDate} type="time"/>
+        <input type="file"/>
+        <input type="image" src={`https://dummyimage.com/500x40/000/ffffff.jpg&text=Input image submit`} alt="Тут должна быть картинка"/>
+        <input type="submit"/>
+        <input type="reset"/>
       </form>
     </div>
   );
